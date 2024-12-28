86d1bc39b94b585141f2a5a0b2cd65a487a3a20d
flar1ngan <flar1ngan@gmail.com>
2024-12-29 01:45:43 +0200

    set reactstrictmode to false

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'img.clerk.com',
-        },        {
-          protocol: 'https',
-          hostname: 'kmnjuogoywbostdbaanh.supabase.co',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "kmnjuogoywbostdbaanh.supabase.co",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
