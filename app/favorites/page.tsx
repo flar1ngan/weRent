ed09a621bfdd46cb155763762e85a014d3854bff
flar1ngan <flar1ngan@gmail.com>
2024-12-28 17:52:31 +0200

    remove favorites test

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,14 +1,14 @@
-import { getFavorites } from "@/utils/actions";
-import EmptyList from "@/components/home/EmptyList";
-import ItemsList from "@/components/home/ItemsList";
+// import { getFavorites } from "@/utils/actions";
+// import EmptyList from "@/components/home/EmptyList";
+// import ItemsList from "@/components/home/ItemsList";
 
-async function FavoritesPage() {
-  const favorites = await getFavorites();
-  if (favorites.length === 0) {
-    return <EmptyList />;
-  }
+// async function FavoritesPage() {
+//   const favorites = await getFavorites();
+//   if (favorites.length === 0) {
+//     return <EmptyList />;
+//   }
 
-  return <ItemsList items={favorites} />;
-}
+//   return <ItemsList items={favorites} />;
+// }
 
-export default FavoritesPage;
+// export default FavoritesPage;
